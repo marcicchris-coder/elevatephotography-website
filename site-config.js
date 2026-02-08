7c14c7a6f19fe93417eab3459e96272ee762dc33
marcicchris-coder <marcic.chris@icloud.com>
2026-02-08 15:19:31 -0500

    Point frontend to live Render API

--- site-config.js
+++ site-config.js
@@ -1,5 +1,5 @@
 window.SITE_LINKS = {
-  api_base: "http://127.0.0.1:8788",
+  api_base: "https://elevatephotography-website.onrender.com",
   order_page: "order.html",
   aryeo_order_form: "https://elevate-real-estate-photography.aryeo.com/order-forms/01991634-25ad-73f1-bf2e-194a7cbb4ff8",
   aryeo_portal: "https://elevate-real-estate-photography.aryeo.com/portal"
